45393889f1e3f1d36ec5fa5d42d30de95f5f4598
Camilo Romero <militoromero10@gmail.com>
2021-06-05 19:31:33 -0500

    adding support for activemq-artemis

---
+++ src/main/java/com/milo/ms/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.milo.ms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    /**
+     * Jackson can serialize and deserialize text message with jms
+     * */
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+}
